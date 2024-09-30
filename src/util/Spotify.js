49d954651a8f9c6a1f6370c2a7dbda7574d756a3
Manu Eliza <degregorioem@gmail.com>
2024-09-30 23:31:05 +0200

    Adjustments in Spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,7 @@
+let accessToken;
 const clientId = '523fa4bcb8d54cad83116e800d8b43bc'; // Insert client ID here.
 const redirectUri = 'https://macowly.netlify.app/'; // Replace this with your actual Netlify URL
-let accessToken;
+//let accessToken;
 
 const Spotify = {
   getAccessToken() {
@@ -13,24 +14,26 @@ const Spotify = {
     if (accessTokenMatch && expiresInMatch) {
       accessToken = accessTokenMatch[1];
       const expiresIn = Number(expiresInMatch[1]);
-      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
       window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
       return accessToken;
-    } else {
+    } //else {
+      // If no access token is present, redirect to Spotify authorization
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
       window.location = accessUrl;
-    }
+    //}
   },
 
   search(term) {
-    const accessToken = Spotify.getAccessToken();
+    //const accessToken = Spotify.getAccessToken();
+    const token = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`
+        Authorization: `Bearer ${token}`
       }
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
+    }).then(response => response.json()
+      //return response.json();
+    ).then(jsonResponse => {
       if (!jsonResponse.tracks) {
         return [];
       }
@@ -49,8 +52,8 @@ const Spotify = {
       return;
     }
 
-    const accessToken = Spotify.getAccessToken();
-    const headers = { Authorization: `Bearer ${accessToken}` };
+    const token = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${token}` };
     let userId;
 
     return fetch('https://api.spotify.com/v1/me', {headers: headers}
